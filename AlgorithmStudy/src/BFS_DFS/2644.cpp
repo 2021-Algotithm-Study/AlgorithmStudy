58167ca9eb398303596e2cea9df88184f07917c8
Replit user <replituser@example.com>
2021-03-21 11:25:39 +0000

    2644 Calculate Family Tree (Silver 2)

---
+++ AlgorithmStudy/src/BFS_DFS/2644.cpp
@@ -0,0 +1,56 @@
+//2644 촌수 계산
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int family[102][102] = {0};
+int check[102] = {0};
+int depth[102] = {0};
+
+int n, m;
+int x, y;
+
+void bfs(int v){
+    queue<int> q;
+    check[v] = 1;
+    q.push(v);
+    while (!q.empty()){
+        v = q.front();
+        q.pop();
+        for(int i = 1; i <= n; i++){
+            if (family[v][i] == 1 && !check[i]){
+                check[i] = 1;
+                depth[i] = depth[v] + 1;
+                q.push(i);
+            }
+        }
+    }
+    
+}
+
+int main(void){
+    cin >> n >> x >> y >> m;
+    
+    int temp1;
+    int temp2;
+    for(int i = 0; i< m; i++){
+        cin >> temp1 >> temp2;
+        family[temp1][temp2] = 1;
+        family[temp2][temp1] = 1;
+    }
+    
+    for(int i = 1; i <= n; i++){
+        
+        for(int j = 1; j <= n; j++){
+            cout << family[i][j];
+        }
+        cout << "\n";
+    }
+    bfs(x);
+    if (depth[y] != 0)
+        cout << depth[y] << endl;
+    else
+        cout << "-1" << endl; 
+    return (0);
+}
