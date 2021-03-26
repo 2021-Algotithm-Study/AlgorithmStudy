1fa365b0a5faf136538d33b2fcfb17a866470b07
J-NINE <wjdrndk96@gmail.com>
2021-03-26 13:47:12 +0900

    16562 친구비 refact

--- AlgorithmStudy/src/BFS_DFS/16562.cpp
+++ AlgorithmStudy/src/BFS_DFS/16562.cpp
@@ -1,52 +1,47 @@
-//16562 친구비
 #include <iostream>
-#include <algorithm>
-#include <vector>
 
 using namespace std;
 
-int parent[100001] = {0};
+int leader[100001] = {0};
 int cost[10001] = {0};
 int n, m, money;
 
-//최상위 parent만큼
-int findParent(int x){
-    if (parent[x] == x){
+int find_leader(int x){
+    if (leader[x] == x)
         return x;
-    }
-    return parent[x] = findParent(parent[x]);
+    return (find_leader(leader[x]));
 }
 
-void unionParent(int x, int y){
-    x = findParent(x);
-    y = findParent(y);
-    if (cost[x] < cost[y])
-        parent[y] = x;
-    else 
-        parent[x] = y;
+//어차피 x y가 친구관계로 묶여 있으므로 둘의 leader cost를 비교해서 그룹을 흡수합병시킨다
+void union_leader(int x, int y){
+    x = find_leader(x);
+    y= find_leader(y);
+    if (cost[x] > cost[y])
+        leader[x] = y;
+    else
+        leader[y] = x;
 }
 
-int main(){
+int main(void){
     cin >> n >> m >> money;
+    //모든 사람의 최초 leader는 자기 자신
     for(int i = 1; i <= n; i++)
-        parent[i] = i;
+        leader[i] = i;
     for(int i =1; i <= n; i++)
         cin >> cost[i];
-    int temp1, temp2;
-    for(int i = 0; i <m; i++){
+    for(int i = 0; i < m; i++){
+        int temp1, temp2;
         cin >> temp1 >> temp2;
-        unionParent(temp1, temp2);
+        union_leader(temp1, temp2);
     }
-   
+
     int total = 0;
-    for(int i = 1; i <= n; i++){
-        int temp = findParent(i);
-        if (temp){
-            //한 번 더해준 최상위 parent는 0과 union해서 0으로 만들어버림
-            //temp 0으로 잡히므로 다음에도 안 더해짐
-            total += cost[temp];
-            unionParent(0, temp);
-        }
+    for(int i = 1; i <=n ; i++){
+        int temp_leader = find_leader(i);
+        if (temp_leader != 0){
+            total += cost[temp_leader];
+            union_leader(0, temp_leader);
+        }    
     }
     if (money >= total)
         cout << total;
