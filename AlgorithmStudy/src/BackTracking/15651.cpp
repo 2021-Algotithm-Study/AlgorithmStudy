dde21c15d5720c8bc5d4e87fa616cf2806256cfb
J-NINE <wjdrndk96@gmail.com>
2021-04-05 15:30:32 +0900

    15651 N과 M (Silver 3)

---
+++ AlgorithmStudy/src/BackTracking/15651.cpp
@@ -0,0 +1,27 @@
+//15651 N°ú M(3) Silver 3
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int arr[9];
+
+void getComb(int ctr){
+    if (ctr == M){
+        for(int i = 0; i < M; i++){
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for(int i = 1; i <= N; i ++){
+            arr[ctr] = i;
+            getComb(ctr+1);
+        }
+}
+
+int main(){
+    cin >> N >> M;
+    getComb(0);
+    return (0);
+}
