1ebc79899edecb145349844da61fe7cbc7035e2d
gojung <wjdrndk96@gmail.com>
2021-07-20 21:59:00 +0900

    1008 A/B(Bronze 4)

---
+++ AlgorithmStudy/java/boj_stepbystep/step1/java_1008.java
@@ -0,0 +1,22 @@
+//1000 A+B(Bronze 5)
+//type casting 들어가더라도 둘 다 buffered 쓰는 게 훨씬 빠르다.  
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class java_1008 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        double a = Double.parseDouble(st.nextToken()) / Double.parseDouble(st.nextToken());
+        bw.write(String.valueOf(a));
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
